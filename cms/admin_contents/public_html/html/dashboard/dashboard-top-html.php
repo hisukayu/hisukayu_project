e1bf0ecb296dcbdb90cea1e8ceec2e866071bf3a
hisukayu <hisukayu@blueclair.jpn.com>
2016-11-27 19:31:49 +0900

    ダッシュボードを更新

--- cms/admin_contents/public_html/html/dashboard/dashboard-top-html.php
+++ cms/admin_contents/public_html/html/dashboard/dashboard-top-html.php
@@ -8,37 +8,76 @@ $admins = SessionLoader::getSessionName("admins");
 </div><!-- #end class box-left -->
 
 <div class="main-box-right" >
-	<section>
-		<h1>ダッシュボード</h1>
-
-		<div id="information" >
-			<section>
-				<h2>お知らせ</h2>
-				<div id="info-lists" >
-					<dl>
-						<dt><span class="date" >2016.11.01</span><span class="info-title" >お知らせタイトルを表示します。</span></dt>
-						<dd>
-							ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。<br />
-							ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。
-						</dd>
-						<dt><span class="date" >2016.11.01</span><span class="info-title" >お知らせタイトルを表示します。</span></dt>
-						<dd>
-							ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。<br />
-							ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。
-						</dd>
-						<dt><span class="date" >2016.11.01</span><span class="info-title" >お知らせタイトルを表示します。</span></dt>
-						<dd>
-							ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。<br />
-							ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。
-						</dd>
-						<dt><span class="date" >2016.11.01</span><span class="info-title" >お知らせタイトルを表示します。</span></dt>
-						<dd>
-							ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。<br />
-							ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。
-						</dd>
-					</dl>
-				</div><!-- #end id info-lists -->
-			</section>
-		</div><!-- #end id information -->
-	</section>
-</div><!-- #end id box-right -->
+	<div class="layout-main-box" >
+		<section>
+			<h1>ダッシュボード</h1>
+			<div class="layout-sub-box" >
+				<div id="information" >
+					<section>
+						<h2>お知らせ</h2>
+						<div id="info-lists" >
+							<dl>
+								<dt>
+									<span class="title-box" >
+										<span class="date" >2016.11.01</span>
+										<span class="buttons" >
+											<span class="edit" ><a href="" >edit</a></span>
+											<span class="delete" ><a href="" >delete</a></span>
+										</span>
+									</span>
+									<span class="info-title" >お知らせタイトルを表示します。</span>
+								</dt>
+								<dd>
+									ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。<br />
+									ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。
+								</dd>
+								<dt>
+									<span class="title-box" >
+										<span class="date" >2016.11.01</span>
+										<span class="buttons" >
+											<span class="edit" ><a href="" >edit</a></span>
+											<span class="delete" ><a href="" >delete</a></span>
+										</span>
+									</span>
+									<span class="info-title" >お知らせタイトルを表示します。</span>
+								</dt>
+								<dd>
+									ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。<br />
+									ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。
+								</dd>
+								<dt>
+									<span class="title-box" >
+										<span class="date" >2016.11.01</span>
+										<span class="buttons" >
+											<span class="edit" ><a href="" >edit</a></span>
+											<span class="delete" ><a href="" >delete</a></span>
+										</span>
+									</span>
+									<span class="info-title" >お知らせタイトルを表示します。</span>
+								</dt>
+								<dd>
+									ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。<br />
+									ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。
+								</dd>
+								<dt>
+									<span class="title-box" >
+										<span class="date" >2016.11.01</span>
+										<span class="buttons" >
+											<span class="edit" ><a href="" >edit</a></span>
+											<span class="delete" ><a href="" >delete</a></span>
+										</span>
+									</span>
+									<span class="info-title" >お知らせタイトルを表示します。</span>
+								</dt>
+								<dd>
+									ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。<br />
+									ここにはお知らせに関する詳細が表示されます。内容を確認してくださいね。
+								</dd>
+							</dl>
+						</div><!-- #end id info-lists -->
+					</section>
+				</div><!-- #end id information -->
+			</div><!-- #end class layout-sub-box -->
+		</section>
+	</div><!-- #end class layout-main-box -->
+</div><!-- #end id box-right and class layout-main-box -->
