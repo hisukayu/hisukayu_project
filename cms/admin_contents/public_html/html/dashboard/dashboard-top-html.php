ae2ce3f3c4491b7f299a708236ddbea72e90d1cf
hisukayu <hisukayu@blueclair.jpn.com>
2016-12-02 17:21:35 +0900

    一旦コミット

--- cms/admin_contents/public_html/html/dashboard/dashboard-top-html.php
+++ cms/admin_contents/public_html/html/dashboard/dashboard-top-html.php
@@ -13,48 +13,54 @@ $infos = InfoPDO::InfoList();
 	<div class="layout-main-box" >
 		<section>
 			<h1>ダッシュボード</h1>
-			<div class="layout-sub-box" >
-				<div id="information" >
-					<section>
-						<h2>お知らせ</h2>
-						<div id="info-lists" >
-							<dl>
-							<?php for($i = 0; $i < count($infos); $i++) :?>
-							<dt>
-								<span class="title-box" >
-									<span class="new" >NEW</span>
-									<span class="date" ><?php echo $infos[$i]['info_regdate']; ?></span>
-									<span class="buttons" >
-										<span class="edit" ><a href="dashboard-edit/<?php echo $infos[$i]['id']; ?>" title="編集する" ><img src="img/common/icon-edit-off.png" alt="編集アイコン" onmouseover="src='img/common/icon-edit-on.png'" onmouseout="src='img/common/icon-edit-off.png'" width="18px" ></a></span>
-										<span class="delete" ><a href="dashboard-delete/<?php echo $infos[$i]['id']; ?>" title="削除する" ><img src="img/common/icon-delete-off.png" alt="削除アイコン" onmouseover="src='img/common/icon-delete-on.png'" onmouseout="src='img/common/icon-delete-off.png'" width="18px" ></a></span>
-									</span>
-								</span>
-								<span class="info-title" ><a href="dashboard-edit-view/<?php echo $infos[$i]['id']; ?>" ><?php echo $infos[$i]['info_title']; ?></a></span>
-							</dt>
-							<dd>
-								<?php echo str_replace(array("\n","\n\r","\r"),"<br />",$infos[$i]['info_detaile']); ?>
-							</dd>
-							<?php endfor; ?>
-							</dl>
-						</div><!-- #end id info-lists -->
-					</section>
-				</div><!-- #end id information -->
-			</div><!-- #end class layout-sub-box -->
+			<div class="box-table margin-T20" >
+				<div class="box-left padding-R20" >
+					<div class="layout-sub-box" >
+						<div id="information" >
+							<section>
+								<h2>お知らせ</h2>
+								<div id="info-lists" >
+									<dl>
+									<?php for($i = 0; $i < count($infos); $i++) :?>
+									<dt>
+										<span class="title-box" >
+											<span class="new" >NEW</span>
+											<span class="date" ><?php echo $infos[$i]['info_regdate']; ?></span>
+											<span class="buttons" >
+												<span class="edit" ><a href="dashboard-edit/<?php echo $infos[$i]['id']; ?>" title="編集する" ><img src="img/common/icon-edit-off.png" alt="編集アイコン" onmouseover="src='img/common/icon-edit-on.png'" onmouseout="src='img/common/icon-edit-off.png'" width="18px" ></a></span>
+												<span class="delete" ><a href="dashboard-delete/<?php echo $infos[$i]['id']; ?>" title="削除する" ><img src="img/common/icon-delete-off.png" alt="削除アイコン" onmouseover="src='img/common/icon-delete-on.png'" onmouseout="src='img/common/icon-delete-off.png'" width="18px" ></a></span>
+											</span>
+										</span>
+										<span class="info-title" ><a href="dashboard-edit-view/<?php echo $infos[$i]['id']; ?>" ><?php echo $infos[$i]['info_title']; ?></a></span>
+									</dt>
+									<dd>
+										<?php echo str_replace(array("\n","\n\r","\r"),"<br />",$infos[$i]['info_detaile']); ?>
+									</dd>
+									<?php endfor; ?>
+									</dl>
+								</div><!-- #end id info-lists -->
+							</section>
+						</div><!-- #end id information -->
+					</div><!-- #end class layout-sub-box -->
+				</div><!-- #end class box-left -->
 
-			<div class="layout-sub-box" >
-				<section>
-					<h2>お知らせ投稿</h2>
-					<div id="" >
-						<div class="forms" >
-							<div class="title" >
-								<span>タイトル</span>
+				<div class="box-right" >
+					<div class="layout-sub-box" >
+						<section>
+							<h2>お知らせ投稿</h2>
+							<div id="info-reg-form" >
+								<div class="forms" >
+									<div class="parts-title" >
+										<span>タイトル</span>
+									</div>
+									<div class="parts-input" >
+										<span><input type="text" name="info-title" value="" ></span>
+									</div>
+								</div>
 							</div>
-							<div class="parts" >
-								<span><input type="" name="" value="" ></span>
-							</div>
-						</div>
-					</div>
-				</section>
+						</section>
+					</div><!-- #end class layout-sub-box -->
+				</div><!-- #end class box-right -->
 			</div>
 		</section>
 	</div><!-- #end class layout-main-box -->
